64a65e40a38353d95ae8bd4b51045816e3ca7a6b
Abdullah Thowzif <thowzif@live.com>
2019-10-17 17:17:45 +0400

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Informatics_Highestshipping',
+    __DIR__
+);
---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_Checkout/js/model/checkout-data-resolver': 'Informatics_Highestshipping/js/model/checkout-data-resolver'
+        }
+    }
+};
